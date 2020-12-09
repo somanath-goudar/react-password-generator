ee936a92607b06fcd7f39e7a7965eea8a9ff20be
Somanath Goudar <somugowda8050@gmail.com>
2020-12-09 11:05:35 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,192 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
+import './App.css'
+import {
+  numbers,
+  upperCaseLetters,
+  lowerCaseLetters,
+  specialCharacters,
+} from './characters'
+import 'react-toastify/dist/ReactToastify.css'
+import { COPY_SUCCESS } from './message'
 
 function App() {
+  const [password, setPassword] = useState('')
+  const [passwordLength, setPasswordLength] = useState(20)
+  const [includeUppercase, setIncludeUppercase] = useState(false)
+  const [includeLowercase, setIncludeLowercase] = useState(false)
+  const [includeNumbers, setIncludeNumbers] = useState(false)
+  const [includeSymbols, setIncludeSymbols] = useState(false)
+
+  const handleGeneratePassword = (e) => {
+    if (
+      !includeUppercase &&
+      !includeLowercase &&
+      !includeNumbers &&
+      !includeSymbols
+    ) {
+      notify('You must Select atleast one option', true)
+    }
+    let characterList = ''
+
+    if (includeLowercase) {
+      characterList = characterList + lowerCaseLetters
+    }
+
+    if (includeUppercase) {
+      characterList = characterList + upperCaseLetters
+    }
+
+    if (includeNumbers) {
+      characterList = characterList + numbers
+    }
+
+    if (includeSymbols) {
+      characterList = characterList + specialCharacters
+    }
+
+    setPassword(createPassword(characterList))
+  }
+  const createPassword = (characterList) => {
+    let password = ''
+    const characterListLength = characterList.length
+
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characterListLength)
+      password = password + characterList.charAt(characterIndex)
+    }
+    return password
+  }
+
+  const copyToClipboard = () => {
+    const newTextArea = document.createElement('textarea')
+    newTextArea.innerText = password
+    document.body.appendChild(newTextArea)
+    newTextArea.select()
+    document.execCommand('copy')
+    newTextArea.remove()
+  }
+
+  const notify = (message, hasError = false) => {
+    if (hasError) {
+      toast.error(message, {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+    } else {
+      toast(message, {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+    }
+  }
+
+  const handleCopyPassword = (e) => {
+    if (password === '') {
+      notify('Nothing To Copy', true)
+    } else {
+      copyToClipboard()
+      notify(COPY_SUCCESS)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <div className='generator'>
+          <h2 className='generator__header'>Password Generator</h2>
+          <div className='generator__password'>
+            <h3>{password}</h3>
+            <button onClick={handleCopyPassword} className='copy__btn'>
+              <i className='far fa-clipboard'></i>
+            </button>
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='password-strength'>Password length</label>
+            <input
+              defaultValue={passwordLength}
+              onChange={(e) => setPasswordLength(e.target.value)}
+              type='number'
+              id='password-strength'
+              name='password-strength'
+              max='20'
+              min='10'
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='uppercase-letters'>Include Uppercase Letters</label>
+            <input
+              checked={includeUppercase}
+              onChange={(e) => setIncludeUppercase(e.target.checked)}
+              type='checkbox'
+              id='uppercase-letters'
+              name='uppercase-letters'
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='lowercase-letters'>Include Lowercase Letters</label>
+            <input
+              checked={includeLowercase}
+              onChange={(e) => setIncludeLowercase(e.target.checked)}
+              type='checkbox'
+              id='lowercase-letters'
+              name='lowercase-letters'
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='include-numbers'>Include Numbers</label>
+            <input
+              checked={includeNumbers}
+              onChange={(e) => setIncludeNumbers(e.target.checked)}
+              type='checkbox'
+              id='include-numbers'
+              name='include-numbers'
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='include-symbols'>Include Symbols</label>
+            <input
+              checked={includeSymbols}
+              onChange={(e) => setIncludeSymbols(e.target.checked)}
+              type='checkbox'
+              id='include-symbols'
+              name='include-symbols'
+            />
+          </div>
+
+          <button onClick={handleGeneratePassword} className='generator__btn'>
+            Generate Password
+          </button>
+          <ToastContainer
+            position='top-center'
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
